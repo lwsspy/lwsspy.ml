64fc77069df95b06f7688e07bcb97f89cf6b6918
Lucas Sawade <lsawade@princeton.edu>
2021-07-02 00:13:22 -0400

    Generated gh-pages for commit 50be316 Author: Lucas Sawade <lsawade@princeton.edu>
        Updated the setup.py, environment.yml etc.

--- src/lwsspy/ml/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Global imports when running the modules for testing
-if "-m" not in sys.argv:
-
-    # Import the functions so that they are accesible from the main module
-    from .labeling.segmentlabeler import SegmentLabeler  # noqa
-    from .labeling.volumelabeler import VolumeLabeler  # noqa
